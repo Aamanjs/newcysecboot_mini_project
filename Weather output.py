204b6e56d0f96518a4ed9c5243f6b649a6498821
Aamanjs <86286688+Aamanjs@users.noreply.github.com>
2021-06-23 00:50:16 -0700

    uploaded the project file

---
+++ Weather output.py
@@ -0,0 +1,8 @@
+Enter the city name: NAVI MUMBAI
+-------------------------------------------------------------
+Weather Stats for - NAVI MUMBAI  || 23 Jun 2021 | 01:00:03 PM
+-------------------------------------------------------------
+Current temperature is: 31.97 deg C
+Current weather desc  : haze
+Current Humidity      : 70 %
+Current wind speed    : 6.69 kmph
